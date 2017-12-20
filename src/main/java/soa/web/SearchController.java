efb91e3557af4e63e78925ae381d9e8a43c50c7a
Enrique Palacin <epalacins@gmail.com>
2017-12-20 14:11:12 +0100

    added max tweets returned in search

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
@@ -23,6 +26,15 @@ public class SearchController {
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        Map<String,Object> headers = new HashMap<>();
+
+        if (q.matches(".+ max:[0-9]+")) {
+            headers.put("CamelTwitterKeywords", q.substring(0, q.lastIndexOf("max:")));
+            headers.put("CamelTwitterCount",q.substring(q.lastIndexOf("max:")+4));
+        } else {
+            headers.put("CamelTwitterKeywords",q);
+        }
+        return producerTemplate.requestBodyAndHeaders("direct:search","",headers);
     }
+
 }
